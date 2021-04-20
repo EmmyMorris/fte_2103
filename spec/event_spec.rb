2092438e37584b3db90586b4d232601e215b7ce1
Emmy Morris <emmymorris1102@gmail.com>
2021-04-20 14:37:09 -0600

    Add food truck

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -15,6 +15,18 @@ RSpec.describe Event do
       event = Event.new("South Pearl Street Farmers Market")
 
       expect(event.name).to eq("South Pearl Street Farmers Market")
+      expect(event.food_trucks).to eq([])
+    end
+  end
+
+  describe 'has food trucks' do
+    it "has food trucks" do
+      event = Event.new("South Pearl Street Farmers Market")
+      food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+
+      event.add_food_truck(food_truck1)
+
+      expect(event.food_trucks).to eq([food_truck1])
     end
   end
 end
